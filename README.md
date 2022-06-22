0e440304314987592ed56beee1adada498c72c8a
CeciliaBSouza <107931419+undefined@users.noreply.github.com>
2022-06-22 18:48:29 +0000

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# lwc-cards-list
-Created with WebComponents.dev
---
+++ src/app.js
@@ -0,0 +1,26 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+
+  numero = [
+      {
+        Id:"849898498ff",
+        Nome:"ceci",
+        Cargo:'Comerciante'
+      },
+      {
+        Id:'849898498ff',
+        Nome:"Maria",
+        Cargo:'Gerente'
+      },
+      {
+        Id:'849898498ff',
+        Nome:"Pedro",
+        Cargo:'caixa'
+
+      }
+
+
+  ];
+
+}
